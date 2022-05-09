12a6a943a9d780fde6bbb7cea31e827d39f9675a
deepali <102019158+theshersha@users.noreply.github.com>
2022-05-09 11:02:33 +0530

    Add files via upload

--- HeaderCartButton.js
+++ HeaderCartButton.js
@@ -1,16 +1,17 @@
-import CartIcon from "../../Cart/CartIcon";
+
+import CartIcon from './CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-    return (
-    <button className = {classes.button} onClick={props.onClick}>
-    <span className = {classes.icon}>
-       <CartIcon/>   
-    </span>
-    <span> Your Cart </span>
-    <span className ={classes.bagde}> 3 </span> 
+  return (
+    <button className={classes.button} onClick={props.onOpen}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>0</span>
     </button>
-    );
+  );
 };
 
 export default HeaderCartButton;
