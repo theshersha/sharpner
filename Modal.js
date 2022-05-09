87d27a88852287da3ef96e65bb6fe3b1e1f3aa04
deepali <102019158+theshersha@users.noreply.github.com>
2022-05-09 11:03:55 +0530

    Add files via upload

--- Modal.js
+++ Modal.js
@@ -1,29 +1,33 @@
-import { Fragment } from "react";
-import ReactDOM  from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
 
-const Backdrop = (props)=> {
- return(
-      <div className={classes.backdrop}/>
-   )
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
-const ModalOverlay =(props) => {
- return(
-  <div className={classes.modal}>
-  <div className={classes.content}>{props.childern}</div>
-  </div>
- );
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+      {/* <button className={classes.content}>Close</button> */}
+    </div>
+  );
 };
 
 const portalElement = document.getElementById('overlays');
 
-const Modal= (props) => {
- return(
+const Modal = (props) => {
+  return (
     <Fragment>
-       {ReactDOM.createPortal(<Backdrop/>,portalElement)}
-       {ReactDOM.createPortal(<ModalOverlay>{props.childern}</ModalOverlay>,portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
+      {console.log("modal is called")}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </Fragment>
- );
+  );
 };
+
 export default Modal;
